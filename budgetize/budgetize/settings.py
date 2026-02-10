4037b7b5b9fa1f4850b497b687189b3de20f03cf
Tinotenda Joshua Matingo <tinotendajoshua2@gmail.com>
2026-02-10 23:16:51 +0200

    reinit of initial commit

--- budgetize/budgetize/settings.py
+++ budgetize/budgetize/settings.py
@@ -11,26 +11,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-import environ 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-env = environ.Env(
-    DEBUG=(bool, False)  # default value if not set
-)
-
-# Read .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-xx_*gyelhv_c_6=4&cw%nujx6^gcc^(y#sz%7*aa87z(k6d+-3'
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -44,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'budgetrack.apps.BudgetrackConfig',
-    'insights.apps.InsightsConfig',
 ]
 
 MIDDLEWARE = [
@@ -83,7 +73,10 @@ WSGI_APPLICATION = 'budgetize.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db_url()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -122,6 +115,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'  # If using crispy forms with Bootstrap 4
--- budgetrack/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Stipend, Expense
-# Register your models here.
-
-admin.site.register(Stipend)
-admin.site.register(Expense)
